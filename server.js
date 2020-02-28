5e0c4e1bcfe4727b18c774395d32620b665bc910
Mark Jenquine <mjenquine@gmail.com>
2020-02-28 15:12:55 -0600

    reverted database back to ohmycrud

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/reading_logs';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ohmycrud';
 
 
 // Error / success
